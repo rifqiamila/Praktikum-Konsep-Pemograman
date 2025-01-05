b9619b729345cd2d89ad67164f271518aa8cfa8b
rifqia <rifqiamila@gmail.com>
2025-01-05 21:49:35 +0700

    Create D, Pengurangan_Seru.c

---
+++ Competitive Programming/Week 2/D, Pengurangan_Seru.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    unsigned int n , k;
+
+    scanf("%d %d", &n, &k);
+
+    for (int i = 1; i <= k; i++) {
+        if (n % 10 == 0) {
+            n = n / 10;
+        }
+        else {
+            n = n - 1;
+        }
+    }
+
+    printf("%d\n", n);
+
+    return 0;
+}
