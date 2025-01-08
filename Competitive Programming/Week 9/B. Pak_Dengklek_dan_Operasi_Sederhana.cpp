0f7d3b76dc590188b25c4766c61f5bedef362fd2
rifqia <rifqiamila@gmail.com>
2025-01-08 20:18:55 +0700

    Create B. Pak_Dengklek_dan_Operasi_Sederhana.cpp

---
+++ Competitive Programming/Week 9/B. Pak_Dengklek_dan_Operasi_Sederhana.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    
+    cin >> x >> y;
+    cout << x + y << endl << x - y << endl << x * y << endl << x / y << endl << x % y;
+    
+    return 0;
+}
